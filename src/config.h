68c136a22bf06d07a914101cce3298bd58e09392
Zach Caldwell <zach.caldwell101@gmail.com>
2020-09-25 14:22:22 -0600

    Implement ways to control emulation speed

--- src/config.h
+++ src/config.h
@@ -11,12 +11,20 @@
 #ifndef _YAMS_CONFIG_H_
 #define _YAMS_CONFIG_H_
 
-// Define the bitwidth of real memory (RAM/ROM) space
+// Define the bitwidth of real memory (RAM/ROM) space.
 // 21 address bits == 2MB
 #define MEMORY_BITS  21
 
-// Define the debug memory address
+// Define the debug memory address.
 // Any value written to this address will be output to the console
 #define DBG_ADDRESS  0xDEC0DE
+
+// Define the simulation refresh speed (in Hz).
+// Helps prevent visual jitter by spacing out cycle use, vs. just quickly
+// burning all the cycles-per-second and delaying the rest of the second
+#define SIM_REFRESH_HZ 10
+
+// Define the emulated CPU's clock speed (in Hz).
+#define CPU_CLOCK_HZ 12000000
 
 #endif //_YAMS_CONFIG_H_
--- src/main.c
+++ src/main.c
@@ -12,12 +12,34 @@
 #include "memory.h"
 #include "usage.h"
 #include "musashi/m68k.h"
+#include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+static const Uint32 updateInterval = 1000 / SIM_REFRESH_HZ;
+static const Uint32 updateCycles = CPU_CLOCK_HZ / SIM_REFRESH_HZ;
+
+// Callback to run the CPU every update interval.
+// Runs in a separate thread, so must be careful not to touch the
+// memory or CPU internals from the main thread after starting.
+Uint32 ExecutionCallback(Uint32 interval, void *param)
+{
+	m68k_execute(updateCycles);
+	return interval;
+}
+
+
+// Still using just the console for now, so temporarily revert SDL_main trickery
+#undef main
 int main(int argc, char* argv[])
 {
     YAMS_PrintHeader();
+
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_TIMER) != 0) {
+		SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+        return -1;
+    }
 
     // Set up memory map and ROM image
     YAMS_params* params = YAMS_ParseArgs(argc, argv);
@@ -30,15 +52,32 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	// Run the simulation
+	// Start the CPU emulation loop
     m68k_init();
     m68k_set_cpu_type(M68K_CPU_TYPE_68010);
     m68k_pulse_reset();
-    m68k_execute(10000);
+	SDL_TimerID CPUTimer = SDL_AddTimer(updateInterval, ExecutionCallback, NULL);
+
+	// Handle any events that come up, and hang this thread in the background otherwise
+    SDL_Event evt;
+	while (1) {
+		if (SDL_PollEvent(&evt)) {
+			// Need to keep this event type outside a switch
+			// so we can break out of the while loop
+			if (evt.type == SDL_QUIT) {
+				SDL_RemoveTimer(CPUTimer);
+				SDL_Delay(updateInterval);
+				break;
+			}
+		}
+		else
+			SDL_Delay(updateInterval);
+	}
 
 	// Clean up
 	YAMS_FreeMemoryMap();
     free(params);
+	SDL_Quit();
     printf("End of simulation");
 
     return 0;
